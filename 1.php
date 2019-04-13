96f9c759aabf72b53e00590ea2ee3ced9e27b372
generalpeanuts <dragneel123@gmail.com>
2019-04-13 16:07:01 +0530

    Update 1.php

--- 1.php
+++ 1.php
@@ -5,7 +5,7 @@
 			$user = $_POST['user'];
 			$pass = $_POST['pass'];
 			if($user == 'admin' && $pass == '1234'){
-				header("Location: upload.php");
+				header("Location: /upload.php");
 			}
 			else{
 				echo "User or pass wrong!";
